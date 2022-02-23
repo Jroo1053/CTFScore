34d538f5c238b8297c2dd39b621f99a49c8851e9
Jroo1053 <jroo1053@gmail.com>
2022-02-23 10:08:25 +0000

    parser fix

--- Source/Web/apiroutes.py
+++ Source/Web/apiroutes.py
@@ -255,7 +255,7 @@ def get_retrive_alerts(id):
                 UserAlert.user_id == current_user.id
             ).filter(
                 UserAlert.alert_id == IDSAlert.id
-            ).limit(10000).all()
+            ).order_by(desc(IDSAlert.timestamp)).limit(10000).all()
             return jsonify(alerts)
         return Response("", 405)
     else:
